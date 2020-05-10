e9b043d5910a3e1a43359c27b270e92f5dc9038e
David Hein <david-hein196@web.de>
2020-05-10 13:30:46 +0200

    Add class to get params

---
+++ automated_repos/params.py
@@ -0,0 +1,9 @@
+class Params(object):
+    def __init__(self, args):
+        self.args = args
+
+        # arg 0
+        try:
+            self.arg0 = self.args[0]
+        except:
+            self.arg0 = None
