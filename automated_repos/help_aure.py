93084599c7bae455a6023552aa06eab62c06c3f7
David Hein <david-hein196@web.de>
2020-05-10 13:31:06 +0200

    Add file with help text

---
+++ automated_repos/help_aure.py
@@ -0,0 +1,19 @@
+'''Automate the work with multiple git repos with this tool.
+
+You can report problems and contribute ideas for improvement to the
+GitHub page at https://github.com/MasterZydra/Automated-Repos
+
+Usage:
+  aure [command]
+
+Available Commands:
+  exec  Generate shell completion scripts
+  job        Help about any command
+  repo      Set and get gh settings
+
+Flags:
+      --help              Show help for command
+      --version           Show gh version
+
+Use "aure [command] --help" for more information about a command.
+'''
