d8e52cc20a539bc38ca326ede00e5e0cf7289418
David Hein <david-hein196@web.de>
2020-05-10 18:49:43 +0200

    Add constants for commands and error messages

---
+++ automated_repos/constants.py
@@ -0,0 +1,10 @@
+# Terminal commands
+GIT_HELP = "git --help"
+GIT_STATUS = "git status"
+GIT_VERSION = "git --version"
+
+# Messages
+ERROR = "Error: "
+MSG_NO_GIT_INSTALLED = "git is not installed or not in the PATH"
+MSG_LANG_NOT_SUPPORTED = "given language is not supported"
+MSG_LANG_IS_UNKNOWN = "git language is unknown"
