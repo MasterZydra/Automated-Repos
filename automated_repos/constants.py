6b83ebd25a1c641f027020341f06b6e233853118
David Hein <david-hein196@web.de>
2020-05-17 22:23:54 +0200

    Add more constants for messages, file names and config separator

--- automated_repos/constants.py
+++ automated_repos/constants.py
@@ -10,3 +10,10 @@ MSG_NO_GIT_INSTALLED = "git is not installed or not in the PATH"
 MSG_LANG_NOT_SUPPORTED = "given language is not supported"
 MSG_LANG_IS_UNKNOWN = "git language is unknown"
 MSG_NOT_A_GIT_REPO = "folder is not git repository"
+MSG_NO_AURE_CONFIG = "no config file ('.aure') found in directory and parent directories"
+
+# File names
+AURE_CONFIG = ".aure"
+
+# Config
+CONFIG_SEPARATOR = "/"
