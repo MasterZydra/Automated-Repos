0d34289bb6051f94fc5ffcbd2a84c90bff917254
David Hein <david-hein196@web.de>
2020-06-22 20:05:44 +0200

    Add message Remove config separator

--- automated_repos/constants.py
+++ automated_repos/constants.py
@@ -11,9 +11,7 @@ MSG_LANG_NOT_SUPPORTED = "given language is not supported"
 MSG_LANG_IS_UNKNOWN = "git language is unknown"
 MSG_NOT_A_GIT_REPO = "folder is not git repository"
 MSG_NO_AURE_CONFIG = "no config file ('.aure') found in directory and parent directories"
+MSG_JOB_NAME_EXPECTED = "Job name expected. Please do not use space in job name."
 
 # File names
 AURE_CONFIG = ".aure"
-
-# Config
-CONFIG_SEPARATOR = "/"
