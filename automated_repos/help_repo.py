1fd48eff1a0cb37793ba62a22c6c602685ffa648
David Hein <david-hein196@web.de>
2020-06-22 23:09:36 +0200

    Add help text

--- automated_repos/help_repo.py
+++ automated_repos/help_repo.py
@@ -1,2 +1,13 @@
-'''Repo help
+'''Automated Repos - Repo
+
+Usage:
+  aure repo [command]
+
+Available Commands:
+  add [path]          Add job [path] to config or create config
+  rem [path]          Remove repository [path]
+  show                Show all repositories
+
+Flags:
+  --help              Show help for command
 '''
