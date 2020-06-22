9c87e3b60ba237c723a43d18cc1c7f4ec2dbe4f0
David Hein <david-hein196@web.de>
2020-06-22 20:04:57 +0200

    Add help text

--- automated_repos/help_job.py
+++ automated_repos/help_job.py
@@ -1,2 +1,19 @@
-'''Job help
+'''Automated Repos - Job
+
+You can report problems and contribute ideas for improvement to the
+GitHub page at https://github.com/MasterZydra/Automated-Repos
+
+Usage:
+  aure job [command]
+
+Available Commands:
+  add [job name]      Add a new job
+  edit [job name]     xyz
+  rem [job name]      xyz
+  show                xyz
+
+Flags:
+  --help              Show help for command
+
+Use "aure job --help" for more information about a command.
 '''
