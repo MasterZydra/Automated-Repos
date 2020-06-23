cd82314a852c81d1cd48903829cd0174cdabe277
David Hein <david-hein196@web.de>
2020-06-23 22:09:55 +0200

    Add logic to load and save repos of a job

--- automated_repos/tomlConfigFile.py
+++ automated_repos/tomlConfigFile.py
@@ -13,20 +13,31 @@ class TomlConfigFile(object):
         # Jobs
         if 'jobs' in tomlObj:
             for job in tomlObj['jobs']:
+                # Add job to AuRe config
                 aureConfig.addJob(job)
+                # Add repos to job
+                if 'repos' in tomlObj['jobs'][job]:
+                    aureConfig.addReposToJob(job, tomlObj['jobs'][job]['repos'])
         
         return aureConfig
 
     @staticmethod
     def writeConfig(config: AureConfig):
-        # Build TOML object
+        # Create empty TOML object
         tomlObj = dict()
-
+        # Add dict for jobs
         tomlObj['jobs'] = dict()
 
-        for job in config.getJobs():
+        jobs = config.getJobs()
+        for job in jobs:
+            # Create jobs
             tomlObj['jobs'][job] = dict()
-        
+            # Add repos
+            tomlObj['jobs'][job]['repos'] = config.getRepos(job)
+
+        # Convert TOML object to TOML string
         tomlStr = toml.dumps(tomlObj)
+
+        # Save TOML string to file
         f = open(constants.AURE_CONFIG, 'w')
         f.write(tomlStr)
