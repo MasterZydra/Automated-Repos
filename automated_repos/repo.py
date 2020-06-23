aca06484bb6352f20fff0af4fd5c7a737d86e65c
David Hein <david-hein196@web.de>
2020-06-23 22:17:35 +0200

    Implement add, remove and show command

--- automated_repos/repo.py
+++ automated_repos/repo.py
@@ -1,6 +1,8 @@
 # Import helper classes
-from .params import Params
+from . import constants
 from . import help_repo
+from .params import Params
+from .repoWrapper import RepoWrapper
 
 class Repo(object):
     def __init__(self, params: Params):
@@ -11,6 +13,29 @@ class Repo(object):
             self.__printHelp()
             return
         
+        # Add
+        elif self.params.arg0 == 'add':
+            if len(self.params.args) != 3:
+                print(constants.ERROR + constants.MSG_PATH_JOB_EXPECTED)
+                return
+
+            RepoWrapper.addRepo(self.params.arg2, self.params.arg1)
+            return
+
+        # Remove
+        elif self.params.arg0 == 'rem':
+            if len(self.params.args) != 3:
+                print(constants.ERROR + constants.MSG_PATH_JOB_EXPECTED)
+                return
+
+            RepoWrapper.remRepo(self.params.arg2, self.params.arg1)
+            return
+
+        # Show
+        elif self.params.arg0 == 'show':
+            RepoWrapper.showRepo()
+            return
+
         else:
             self.__printHelp()
             return
